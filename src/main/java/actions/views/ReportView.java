4aa06a3d84f32a030f4cdfeb45b3599d481e6f10
taira.nagaoka <tairanagaoka035@gmail.com>
2022-06-28 16:54:55 +0900

    Reportモデル、ReportViewモデル作成

---
+++ src/main/java/actions/views/ReportView.java
@@ -0,0 +1,36 @@
+package actions.views;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * 日報情報について画面の入力値・出力値を扱うViewモデル
+ *
+ */
+@Getter //全てのクラスフィールドについてgetterを自動生成する(Lombok)
+@Setter //全てのクラスフィールドについてsetterを自動生成する(Lombok)
+@NoArgsConstructor //引数なしコンストラクタを自動生成する(Lombok)
+@AllArgsConstructor //全てのクラスフィールドを引数にもつ引数ありコンストラクタを自動生成する(Lombok)
+public class ReportView {
+
+    private Integer id;
+
+    private EmployeeView employee;
+
+    private LocalDate reportDate;
+
+    private String title;
+
+    private String content;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+
+}
